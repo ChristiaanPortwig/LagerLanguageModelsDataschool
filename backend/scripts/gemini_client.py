5ba4d594ee2ad178516ab6b9bb13b5d50aa00d67
Johannes Christiaan Portwig <28264045@sun.ac.za>
2026-08-09 12:40:51 +0200

    feat: initial gemini working

--- backend/scripts/gemini_client.py
+++ backend/scripts/gemini_client.py
@@ -1,2 +1,23 @@
-class gemini_client:
-    pass
+import os
+from dotenv import load_dotenv
+from google import genai
+
+load_dotenv()
+
+class Gemini_Client:
+    GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
+
+    client = genai.Client(api_key=GEMINI_API_KEY)
+
+    def call_gemini(self, prompt, model = "gemini-3.5-flash-lite"):
+        interaction = self.client.interactions.create(
+            model = model,
+            input = prompt
+        )
+
+        print(interaction.output_text)
+
+
+if __name__ == "__main__":
+    my_client = Gemini_Client()
+    my_client.call_gemini("This is a test, respond with something cool")
